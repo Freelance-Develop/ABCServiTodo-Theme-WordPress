f30722bce65efffaa0a23af6cab109bb626c9c2d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-03-04 10:18:26 -0500

    [+] Personaliza Tema Base

--- functions.php
+++ functions.php
@@ -1,18 +1,18 @@
 <?php
-    if ( ! function_exists( 'jango_scripts' ) ) :
-        function jango_scripts() {
+    if ( ! function_exists( 'jtabcst_scripts' ) ) :
+        function jtabcst_scripts() {
             wp_enqueue_style(
                 'jango-css',
                 get_template_directory_uri(). '/dist/assets/css/master.min.css'
             );
             wp_enqueue_script(
                 'jango-js',
-                get_theme_file_uri( '/dist/assets/js/build.min.js' ), 
+                get_theme_file_uri( '/dist/assets/js/build.min.js' ),
                 array(),
                 '0.1',
                 true
             );
         }
-        add_action( 'wp_enqueue_scripts', 'jango_scripts' );
-    endif; // jango_scripts
+        add_action( 'wp_enqueue_scripts', 'jtabcst_scripts' );
+    endif; // jtabcst_scripts
 ?>
